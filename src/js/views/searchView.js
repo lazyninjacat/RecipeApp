83574cda6a62b99971d098a0569ab6abda33648c
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-05-14 00:28:18 -0700

    progress toward search view implementation

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -44,6 +44,57 @@ const renderRecipe = recipe => {
     elements.searchResultList.insertAdjacentHTML('beforeend', markup);
 }
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+//type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page -1 : page + 1} >
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+        <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
+    </button>
+
+`;
+
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+
+    if (page === 1 && pages > 1){
+        //Only button to go to next page
+        button = createButton(page, 'next');
+
+    } else if (page < pages){
+        // Both buttons
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `;
+        
+
+
+    } else if (page === pages && pages > 1){
+        // Only button to go to previous page
+        button = createButton(page, 'prev');
+
+    }
+
+    elements.searchResultsPages.insertAdjacentHTML('afterbegin', button);
+
+
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render results of current page
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render pagination buttons
+    renderButtons(page, recipes.length, resPerPage);)
+
 };
